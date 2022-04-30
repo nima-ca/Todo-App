cbeee0e74f7f8702250d0582dfc207aaa159447f
Nima Nekouei <nekoueinima@protonmail.com>
2022-05-01 01:52:06 +0430

    button added and styled

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,9 +1,13 @@
 import styles from "./Task.module.css";
+import Button from "../UI/Button";
 
 const Task = (props) => {
   return (
     <section className={styles.tasks}>
-      <div className={styles["tasks__items"]}></div>
+      <div className={styles["tasks__items"]}>
+        <div></div>
+        <Button content={"Add New Task"} />
+      </div>
       <div className={styles["tasks__panel"]}></div>
     </section>
   );
