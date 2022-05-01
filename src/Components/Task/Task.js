3e3fbef988f4767d705fe071d808da27a5444a7f
Nima Nekouei <nekoueinima@protonmail.com>
2022-05-01 12:53:16 +0430

    task Item component created and styled and dynamically displayed

--- src/Components/Task/Task.js
+++ src/Components/Task/Task.js
@@ -1,11 +1,23 @@
 import styles from "./Task.module.css";
 import Button from "../UI/Button";
+import TaskItem from "./TaskItem";
+
+const Dummy_data = [
+  { title: "Task 1", status: "Done", id: "task-1" },
+  { title: "Task 2", status: "UnDone", id: "task-2" },
+  { title: "Task 3", status: "Done", id: "task-3" },
+  { title: "Task 4", status: "Done", id: "task-4" },
+];
 
 const Task = (props) => {
   return (
     <section className={styles.tasks}>
       <div className={styles["tasks__items"]}>
-        <div></div>
+        <div>
+          {Dummy_data.map((item) => (
+            <TaskItem key={item.id} title={item.title} status={item.status} />
+          ))}
+        </div>
         <Button content={"Add New Task"} />
       </div>
       <div className={styles["tasks__panel"]}></div>
---
+++ src/Components/Task/TaskItem.js
@@ -0,0 +1,20 @@
+import styles from "./TaskItem.module.css";
+
+const TaskItem = (props) => {
+  const { title, status } = props;
+
+  const statusClasses = `${styles.dot} ${
+    status === "Done" ? styles["dot__green"] : styles["dot__red"]
+  }`;
+
+  console.log(status);
+
+  return (
+    <div className={styles.taskItem}>
+      <p>{title}</p>
+      <span className={statusClasses}></span>
+    </div>
+  );
+};
+
+export default TaskItem;
