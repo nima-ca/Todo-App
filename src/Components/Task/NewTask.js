226c75daff55343e306a29c374a195b11527e5ef
Nima Nekouei <nekoueinima@protonmail.com>
2022-05-03 16:20:16 +0430

    data fetched from inputs and a style issue fixed

--- src/Components/Task/NewTask.js
+++ src/Components/Task/NewTask.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Modal from "../UI/Modal/Modal";
 import Input from "../UI/Input/Input";
 import Button from "../UI/Button";
@@ -5,22 +6,68 @@ import { useDispatch } from "react-redux";
 import { toggleModalActions } from "../../store/ModalToggleSlice";
 
 const NewTask = (props) => {
+  const [inputValue, setInputValue] = useState({});
+  const [submitError, setSubmitError] = useState(false);
+
   const dispatch = useDispatch();
   const CancelClickHandler = () => {
     dispatch(toggleModalActions.toggleModal());
   };
 
+  const inputChangeHandler = (data) => {
+    setInputValue((prevState) => {
+      return {
+        ...prevState,
+        [data.id]: {
+          value: data.value,
+          isValid: data.isValid,
+        },
+      };
+    });
+  };
+
+  const formIsValid = () => {
+    let isValid = true;
+    for (let key in inputValue) {
+      if (!inputValue[key].isValid) {
+        isValid = false;
+      }
+    }
+    return isValid;
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid()) {
+      setSubmitError(true);
+      return;
+    }
+
+    const formData = {
+      title: inputValue.title.value,
+      description: inputValue.description.value,
+      status: "UnDone",
+    };
+
+    console.log(formData);
+  };
+
   return (
     <Modal>
       <h1>New Task</h1>
-      <form>
+      <form onSubmit={formSubmitHandler}>
         <Input
+          onSubmit={submitError}
+          onChange={inputChangeHandler}
           id="title"
           label="Title:"
           type="text"
           placeholder="New Task..."
         />
         <Input
+          onSubmit={submitError}
+          onChange={inputChangeHandler}
           id="description"
           label="Description:"
           type="text"
--- src/Components/UI/Input/Input.js
+++ src/Components/UI/Input/Input.js
@@ -1,10 +1,45 @@
+import React, { useState, useEffect } from "react";
 import styles from "./Input.module.css";
 
+const isNotEmpty = (value) => value.trim() !== "";
+
 const Input = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+  const [isValid, setIsValid] = useState(false);
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const valueBlurHandler = () => {
+    setIsTouched(true);
+  };
+
+  const hasError = !isValid && (isTouched || props.onSubmit);
+
+  useEffect(() => {
+    setIsValid(isNotEmpty(enteredValue));
+    props.onChange({
+      id: props.id,
+      value: enteredValue,
+      isValid,
+    });
+  }, [enteredValue, isValid]);
+
+  const inputClasses = `${styles.input} ${hasError ? styles.invalid : ""}`;
+
   return (
     <div className={styles.inputContainer}>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type={props.type} id={props.id} placeholder={props.placeholder} />
+      <input
+        className={inputClasses}
+        onChange={valueChangeHandler}
+        onBlur={valueBlurHandler}
+        type={props.type}
+        id={props.id}
+        placeholder={props.placeholder}
+      />
     </div>
   );
 };
